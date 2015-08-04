b580f05193406203dad281a3c6403e72765bbb05
renegens <renegens@gmail.com>
2015-08-04 17:31:37 +0300

    Initial Commit

---
+++ SimpleTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTimer
+//
+//  Created by Giwrgos Gens on 04/08/15.
+//  Copyright © 2015 Giwrgos Gens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
